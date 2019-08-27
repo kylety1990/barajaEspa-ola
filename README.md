e728f888ebef0b6dbb1335c07eb1af07542c947a
Christian <cristian001_13@hotmail.com>
2019-08-27 16:20:00 +0200

    modification function createDefaultDeck

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+
+//Practicando codigo!
--- app.js
+++ app.js
@@ -1,46 +1,70 @@
 // Creemos recrear una app para, que te cree una baraja de cartas!
 
-function createLetterDefault(arr) {
-    let arrO = [];
-    let arrE = [];
-    let arrB = [];
-    let arrC = [];
+function createLetterDefault() {
+    let arr = [];
 
     for (i = 1; i <= 13; i++) {
 
 
         if (i === 11) {
-            arrO.push("J-O");
-            arrE.push("J-E");
-            arrB.push("J-B");
-            arrC.push("J-C");
+            arr.push("J-O");
+            arr.push("J-E");
+            arr.push("J-B");
+            arr.push("J-C");
         } else if (i === 12) {
-            arrO.push("CO");
-            arrE.push("C-E");
-            arrB.push("C-B");
-            arrC.push("C-C");
+            arr.push("CO");
+            arr.push("C-E");
+            arr.push("C-B");
+            arr.push("C-C");
         } else if (i === 13) {
-            arrO.push("R-O");
-            arrE.push("R-E");
-            arrB.push("R-B");
-            arrC.push("R-C");
+            arr.push("R-O");
+            arr.push("R-E");
+            arr.push("R-B");
+            arr.push("R-C");
         } else {
-            arrO.push(i + "-O");
-            arrE.push(i + "-E");
-            arrB.push(i + "-B");
-            arrC.push(i + "-C");
+            arr.push(i + "-O");
+            arr.push(i + "-E");
+            arr.push(i + "-B");
+            arr.push(i + "-C");
         }
     }
-    arr.push(arrO);
-    arr.push(arrE);
-    arr.push(arrB);
-    arr.push(arrC);
+
     return arr;
 }
 
+function customizedDeck(arrRemove) {
+    let arrO = [];
+    let arrE = [];
+    let arrB = [];
+    let arrC = [];
+
+    for (i = 0; i < arrRemove.length; i++) {
+        console.log(arrRemove[i]);
 
 
-oros = [];
-createLetterDefault(oros);
 
-console.log(oros);
+
+
+    }
+    return arrRemove;
+}
+
+// let toma = [8, 9]
+// let datos = customizedDeck(toma);
+
+// console.log(datos);
+
+// let basura = [1, 8, [3, 5]];
+// console.log(basura.indexOf(3));
+
+let dato = createLetterDefault().indexOf("J-O");
+
+console.log(dato);
+
+// for (i = 0; i < oros.length; i++) {
+
+//     for (j = 0; j < oros[i].length; j++)
+//         console.log(oros[i][j]);
+// }
+
+// console.log(oros[1][1]);
